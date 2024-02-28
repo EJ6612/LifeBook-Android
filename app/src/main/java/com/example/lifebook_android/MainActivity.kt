d1ad00642d9b65dfa0c4aded385f3d1c04101d2d
Elijah Dodge <ej_6612@outlook.com>
2024-02-27 19:02:49 -0700

    Sync
    Upload changes to work on Linux

--- app/src/main/java/com/example/lifebook_android/MainActivity.kt
+++ app/src/main/java/com/example/lifebook_android/MainActivity.kt
@@ -2,36 +2,18 @@ package com.example.lifebook_android
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lifebook_android.ui.theme.LifeBookAndroidTheme
 import java.io.FileOutputStream
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     companion object {
         val FILE_NAME = "lifeBook.txt"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LifeBookAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 
 
@@ -52,21 +34,5 @@ class MainActivity : ComponentActivity() {
 
     fun loadCurrentEvents(v: View) {
 
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LifeBookAndroidTheme {
-        Greeting("Android")
     }
 }
