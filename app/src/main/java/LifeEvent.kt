e801c35ff014900f46d6bd847e3fea46eabf9d2e
kaitlyn-myers8208 <kaitlyn.myers8208@gmail.com>
2024-03-03 17:29:47 -0700

    Added properties for the eventClass

--- app/src/main/java/LifeEvent.kt
+++ app/src/main/java/LifeEvent.kt
@@ -2,5 +2,22 @@ open class LifeEvent {
     var eventName: String = ""
         get() = field
         set(value) { field = value }
-    
+    var eventDate: String = ""
+        get() = field
+        set(value) { field = value }
+    var eventDetails: String = ""
+        get() = field
+        set(value) { field = value }
+    var eventLocation: String = ""
+        get() = field
+        set(value) { field = value }
+
+    var taskList: MutableList<LifeEvent> = mutableListOf()
+    fun saveEvent(newTask: LifeEvent) {
+        taskList.add(newTask)
+    }
 }
+
+// properties -> info
+    // name of event, time/date, explanation, location
+// save to list
